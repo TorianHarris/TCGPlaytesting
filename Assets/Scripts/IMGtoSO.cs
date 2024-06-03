22a249100a08214d6f3f522ce89e99b1421a261d
tharris <torianharris917@gmail.com>
2024-06-02 22:49:30 -0400

    added Image to Scriptable Object

---
+++ Assets/Scripts/IMGtoSO.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class IMGtoSO : MonoBehaviour
+{
+    //string imageFolderPath = "Assets/zz_Images_To_Construct";
+    string scriptableObjectFolderPath = "Assets/zz_Constructed_Cards_To_Sort/";
+    [InspectorButton("GenerateCards", ButtonWidth = 160)]
+    public bool Generate;
+    public List<Sprite> sprites = new List<Sprite>();
+
+    public void GenerateCards()
+    {
+
+
+        foreach (Sprite sprite in sprites)
+        {
+            Card card = ScriptableObject.CreateInstance<Card>();
+            AssetDatabase.CreateAsset(card,$"{scriptableObjectFolderPath}/{sprite.name}.asset");
+            card.name = sprite.name;
+            card.cardArt = sprite;
+        }
+    }
+}
